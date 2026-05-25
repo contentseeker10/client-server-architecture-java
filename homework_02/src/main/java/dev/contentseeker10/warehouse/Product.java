83090d820c371fa3648ce72b89b31a5223381f87
contentseeker10 <appelsene.design@gmail.com>
2026-05-25 09:44:43 +0300

    add setters to entities

--- homework_02/src/main/java/dev/contentseeker10/warehouse/Product.java
+++ homework_02/src/main/java/dev/contentseeker10/warehouse/Product.java
@@ -29,16 +29,32 @@ public class Product {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public int getQuantity() {
         return quantity;
     }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public double getPrice() {
         return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     @Override
--- homework_02/src/main/java/dev/contentseeker10/warehouse/ProductGroup.java
+++ homework_02/src/main/java/dev/contentseeker10/warehouse/ProductGroup.java
@@ -29,12 +29,24 @@ public class ProductGroup {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public Set<Product> getProductsList() {
         return productsList;
+    }
+
+    public void setProductsList(Set<Product> productsList) {
+        this.productsList = productsList;
     }
 
     @Override
