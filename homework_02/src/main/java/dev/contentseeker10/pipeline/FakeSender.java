17ca948bca28e539905ee1fb242fb1f60c68c592
contentseeker10 <appelsene.design@gmail.com>
2026-05-25 12:40:21 +0300

    implement FakeSender

--- homework_02/src/main/java/dev/contentseeker10/pipeline/FakeSender.java
+++ homework_02/src/main/java/dev/contentseeker10/pipeline/FakeSender.java
@@ -1,5 +1,37 @@
 package dev.contentseeker10.pipeline;
 
-public class FakeSender {
+import dev.contentseeker10.packet.Decriptor;
+import dev.contentseeker10.packet.Message;
 
+import java.net.InetAddress;
+import java.util.concurrent.BlockingQueue;
+
+public class FakeSender implements Sender, Runnable {
+
+    private BlockingQueue<byte[]> sendQueue;
+
+    public FakeSender(BlockingQueue<byte[]> sendQueue) {
+        this.sendQueue = sendQueue;
+    }
+
+    @Override
+    public void sendMessage(byte[] mess, InetAddress target) {
+        Message response = Decriptor.decript(mess);
+        System.out.println("[SENDER] Sent response for Message ID: "
+                        + response.getMessageId()
+                        + "Status: "
+                        + response.getPayload().getData());
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                sendMessage(sendQueue.poll(), null);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
--- homework_02/src/main/java/dev/contentseeker10/pipeline/Sender.java
+++ homework_02/src/main/java/dev/contentseeker10/pipeline/Sender.java
@@ -1,5 +1,5 @@
 package dev.contentseeker10.pipeline;
 
 public interface Sender {
-
+    void sendMessage(byte[] mess, java.net.InetAddress target);
 }
