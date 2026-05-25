46ad66e4a25e16295df9ef2d45e6a13841d2ee15
contentseeker10 <appelsene.design@gmail.com>
2026-05-25 12:00:51 +0300

    implement FakeReceiver with CSV format output

--- homework_02/src/main/java/dev/contentseeker10/pipeline/FakeReceiver.java
+++ homework_02/src/main/java/dev/contentseeker10/pipeline/FakeReceiver.java
@@ -1,5 +1,97 @@
 package dev.contentseeker10.pipeline;
 
-public class FakeReceiver {
+import dev.contentseeker10.packet.*;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class FakeReceiver implements Receiver, Runnable {
+
+    private BlockingQueue<byte[]> rawQueue;
+
+    private final AtomicLong packetIdSequence = new AtomicLong(0);
+
+    public FakeReceiver(BlockingQueue<byte[]> rawQueue) {
+        this.rawQueue = rawQueue;
+    }
+
+    @Override
+    public void receiveMessage() {
+        CommandType[] types = CommandType.values();
+        CommandType randomType = types[random.nextInt(types.length)];
+        Payload payload = buildPayload(randomType);
+        Message message = new Message(Encoder.MAGIC, (byte) 1, packetIdSequence.incrementAndGet(), payload);
+        byte[] encodedData = Encoder.encode(message);
+        try {
+            rawQueue.put(encodedData);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Payload buildPayload(CommandType randomType) {
+        int userId = 1;
+        String payload = switch (randomType) {
+            case GET_AMOUNT -> generateGetAmountPayload();
+            case WRITE_OFF -> generateWriteOffPayload();
+            case WRITE_ON -> generateWriteOnPayload();
+            case ADD_GROUP -> generateAddGroupPayload();
+            case ADD_PRODUCT_TO_GROUP -> generateAddProductToGroupPayload();
+            case SET_PRICE -> generateSetPricePayload();
+        };
+        return new Payload(randomType.getCode(), userId, payload);
+    }
+
+    private String generateGetAmountPayload() {
+        int id = random.nextInt(5) + 1;
+        return String.valueOf(id);
+    }
+
+    private String generateWriteOffPayload() {
+        int id = random.nextInt(5) + 1;
+        int amount = random.nextInt(10) + 1;
+        return id + "," + amount;
+    }
+
+    private String generateWriteOnPayload() {
+        int id = random.nextInt(5) + 1;
+        int amount = random.nextInt(10) + 1;
+        return id + "," + amount;
+    }
+
+    private String generateAddGroupPayload() {
+        String name = "ProductGroup_" + random.nextInt(100);
+        String description = "some cool product group description for " + name;
+        return name + "," + description;
+    }
+
+    private String generateAddProductToGroupPayload() {
+        int id = random.nextInt(5) + 1;
+        String name = "SomeProduct_" + random.nextInt(100);
+        String description = "man this is really cool product named " + name;
+        double price = 99.99;
+        return id + "," + name + "," + description + "," + price;
+    }
+
+    private String generateSetPricePayload() {
+        int id = random.nextInt(5) + 1;
+        double price = 540.00;
+        return id + "," + price;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                receiveMessage();
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private final Random random = new Random();
 }
--- homework_02/src/main/java/dev/contentseeker10/pipeline/Receiver.java
+++ homework_02/src/main/java/dev/contentseeker10/pipeline/Receiver.java
@@ -1,5 +1,5 @@
 package dev.contentseeker10.pipeline;
 
 public interface Receiver {
-
+    void receiveMessage();
 }
